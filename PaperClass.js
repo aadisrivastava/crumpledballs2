7593e5d1c797218f3b1d0bd13e2f49396120ca84
aadisrivastava <67217741+aadisrivastava@users.noreply.github.com>
2020-09-20 17:08:49 +0530

    Add files via upload

---
+++ PaperClass.js
@@ -0,0 +1,33 @@
+class paperClass{
+    constructor(x,y,radius) {
+        var options = {
+            restitution:0.3,
+            friction:0.5,
+            density:3,
+            isStatic:false
+
+        }
+        this.body = Bodies.circle(x,y, radius, options);
+        this.image=loadImage("paper.png");
+       
+        World.add(world, this.body);
+      }
+    
+    display(){
+
+     var paperSprite=this.body.position;
+      push()
+      translate(paperSprite.x,paperSprite.y);
+      ellipseMode(RADIUS);
+       
+      strokeWeight(3);
+      fill(255,0,255)
+      ellipse(0,0,50,50);
+
+      pop();
+
+
+
+
+    }
+}
